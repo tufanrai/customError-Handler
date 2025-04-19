63b63e80923afbde5d9cd11ab7fd04f431018dbe
TufanRai <thulungtufan16@gmail.com>
2025-04-19 18:29:30 +0545

    error middleware responce updated

--- Controller/error-middleware/error.js
+++ Controller/error-middleware/error.js
@@ -1,3 +1,10 @@
 exports.errorMiddleware = (err,req,res,next) =>{
     res.json(err)
+    const statuscode = err.statusCode || 500
+    const message = err.message || 'faile'
+    res.status(statuscode).json({
+        status: err.status?? 'error',
+        success: err.success?? false,
+        message
+    })
 }
